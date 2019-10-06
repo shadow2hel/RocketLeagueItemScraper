a0703efe0d6100d2222c4c9a5d8ff589c21a66e9
Antonio Carrano <antonio.carrano@student.kdg.be>
2019-10-06 20:02:25 +0200

    Fixed missing items

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 let Items = {};
 
-function writeToJSON(object){
-    let data = JSON.stringify(object, null, 2);
+function writeToJSON(){
+    let data = JSON.stringify(Items, null, 2);
 
     fs.writeFile('items.json', data, (err) => {
         if(err) throw err;
@@ -12,31 +12,19 @@ function writeToJSON(object){
     });
 }
 
-function loopHtml(itemID){
-    setTimeout(request, 500 * itemID, 'https://rocket-league.com/trading?filterItem=' + itemID + '&filterCertification=0&filterPaint=0&filterPlatform=0', function(error, response, html) {
+function loopHtml(){
+    setTimeout(request, 0, 'https://rocket-league.com/trading?filterItem=1000&filterCertification=0&filterPaint=0&filterPlatform=0', function(error, response, html) {
         if (!error && response.statusCode == 200) {
             const $ = cheerio.load(html);
-            found = false;
-            $(".rlg-trade-display-items").each(function () {
-                $(this).each(() => {
-                    let foundHtml = $(this).find('a').attr('href').match(/filterItem=[(0-9)]+/g)[0];
-                    let foundId = foundHtml.substr(foundHtml.indexOf('=') + 1);
-                    let foundName = $(this).find('img').attr('alt');
-                    if (!found && itemID === parseInt(foundId)) {
-                        console.log(foundId + ": " + foundName);
-                        Items[foundName] = foundId;
-                        writeToJSON(Items);
-                        found = true;
 
-                    }
-                });
-            });
+            $('#filterItem').find('option').each(function(i, item){
+                console.log($(item).text() + ": " + $(item).attr('value'))
+                Items[$(item).text()] = $(item).attr('value');
+            })
+            writeToJSON();
         }
     });
 }
 
-var i;
-for(i = 1; i < 3000; i++){
-        loopHtml(i);
-}
+loopHtml();
 
